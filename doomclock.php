15206c24930a283eb1ac96a084bc994f9017224a
Josh Clark <toastyghost@live.com>
2014-06-25 03:51:00 +0100

    changed input type to datetime

--- doomclock.php
+++ doomclock.php
@@ -24,7 +24,7 @@ class DoomClockWidget extends WP_Widget {
 		?>
 		
 		<p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
-		<p><label for="<?php echo $this->get_field_id('date'); ?>">Date: <input class="widefat" id="<?php echo $this->get_field_id('date'); ?>" name="<?php echo $this->get_field_name('date'); ?>" type="date" value="<?php echo attribute_escape($date); ?>" /></label></p>
+		<p><label for="<?php echo $this->get_field_id('date'); ?>">Date: <input class="widefat" id="<?php echo $this->get_field_id('date'); ?>" name="<?php echo $this->get_field_name('date'); ?>" type="datetime" value="<?php echo attribute_escape($date); ?>" /></label></p>
 		
 		<?php
 	}
